0944e4cb3b5a92a30a03e2b49e823851dd41344e
JO HYODONG <160843817+hyodongg@users.noreply.github.com>
2026-04-02 14:59:39 +0900

    [Feat] 메뉴 옵션 그룹 엔티티

--- src/main/java/dgu/capstone/nunchi/domain/menu/entity/MenuOptionGroup.java
+++ src/main/java/dgu/capstone/nunchi/domain/menu/entity/MenuOptionGroup.java
@@ -1,4 +1,28 @@
 package dgu.capstone.nunchi.domain.menu.entity;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "menu_option_group")
 public class MenuOptionGroup {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long optionGroupId;
+
+    @Column(length = 100, nullable = false)
+    private String name;
+
+    private Boolean isRequired;
+
+    private Integer maxSelect;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "menu_id", nullable = false)
+    private Menu menu;
 }
