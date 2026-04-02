1d3e04939fe02f956d6b4dd21bac037160740db2
JO HYODONG <160843817+hyodongg@users.noreply.github.com>
2026-04-02 14:59:24 +0900

    [Feat] 메뉴 엔티티

--- src/main/java/dgu/capstone/nunchi/domain/menu/entity/Menu.java
+++ src/main/java/dgu/capstone/nunchi/domain/menu/entity/Menu.java
@@ -1,4 +1,33 @@
 package dgu.capstone.nunchi.domain.menu.entity;
 
-public class Menu {
+import dgu.capstone.nunchi.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "menu")
+public class Menu extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long menuId;
+
+    @Column(length = 100)
+    private String name;
+
+    private Integer price;
+
+    private Boolean isSoldOut;
+
+    private String imageUrl;
+
+    private Boolean isRecommended;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id", nullable = false)
+    private MenuCategory category;
 }
