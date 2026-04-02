d8029d6705e5fc28bbe081e31369c57cebc501e0
JO HYODONG <160843817+hyodongg@users.noreply.github.com>
2026-04-02 14:59:30 +0900

    [Feat] 메뉴 카테고리 엔티티

--- src/main/java/dgu/capstone/nunchi/domain/menu/entity/MenuCategory.java
+++ src/main/java/dgu/capstone/nunchi/domain/menu/entity/MenuCategory.java
@@ -1,4 +1,23 @@
 package dgu.capstone.nunchi.domain.menu.entity;
 
-public class MenuCategory {
+import dgu.capstone.nunchi.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "menu_category")
+public class MenuCategory extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long categoryId;
+
+    @Column(length = 50, nullable = false)
+    private String name;
+
+    private Integer displayOrder;
 }
