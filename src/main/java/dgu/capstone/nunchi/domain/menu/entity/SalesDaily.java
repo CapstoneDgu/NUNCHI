52c30e82b0ea6a1c80da5b548817c861e70897d4
JO HYODONG <160843817+hyodongg@users.noreply.github.com>
2026-04-02 14:59:46 +0900

    [Feat] 판매 집계 엔티티

--- src/main/java/dgu/capstone/nunchi/domain/menu/entity/SalesDaily.java
+++ src/main/java/dgu/capstone/nunchi/domain/menu/entity/SalesDaily.java
@@ -1,4 +1,33 @@
 package dgu.capstone.nunchi.domain.menu.entity;
 
-public class SalesDaily {
+import dgu.capstone.nunchi.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "sales_daily")
+public class SalesDaily extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDate salesDate;
+
+    @Column(nullable = false)
+    private Integer quantitySold;
+
+    @Column(nullable = false)
+    private Integer salesAmount;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "menu_id")
+    private Menu menu;
 }
