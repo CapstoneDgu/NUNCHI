89aae3a7a1a1da797bdef17b4cf5caf7770beecb
JO HYODONG <160843817+hyodongg@users.noreply.github.com>
2026-04-02 15:17:43 +0900

    [Feat] 결제 엔티티

---
+++ src/main/java/dgu/capstone/nunchi/domain/order/entity/OrderStatus.java
@@ -0,0 +1,7 @@
+package dgu.capstone.nunchi.domain.order.entity;
+
+public enum OrderStatus {
+    PENDING,    // 주문 대기
+    COMPLETED,  // 주문 완료
+    CANCELLED   // 주문 취소
+}
---
+++ src/main/java/dgu/capstone/nunchi/domain/payment/entity/PaymentStatus.java
@@ -0,0 +1,7 @@
+package dgu.capstone.nunchi.domain.payment.entity;
+
+public enum PaymentStatus {
+    PENDING,    // 결제 대기
+    SUCCESS,    // 결제 성공
+    FAILED      // 결제 실패
+}
