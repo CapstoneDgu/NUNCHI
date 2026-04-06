30bd9b00a105f61546cfb5fe9b9c703b713f3a3b
JO HYODONG <160843817+hyodongg@users.noreply.github.com>
2026-04-06 18:39:45 +0900

    [Feat] 메뉴 Repository layer

--- src/main/java/dgu/capstone/nunchi/domain/menu/repository/MenuOptionGroupRepository.java
+++ src/main/java/dgu/capstone/nunchi/domain/menu/repository/MenuOptionGroupRepository.java
@@ -3,5 +3,10 @@ package dgu.capstone.nunchi.domain.menu.repository;
 import dgu.capstone.nunchi.domain.menu.entity.MenuOptionGroup;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MenuOptionGroupRepository extends JpaRepository<MenuOptionGroup, Long> {
+
+    // 메뉴 ID로 옵션 그룹 목록 조회
+    List<MenuOptionGroup> findByMenu_MenuId(Long menuId);
 }
