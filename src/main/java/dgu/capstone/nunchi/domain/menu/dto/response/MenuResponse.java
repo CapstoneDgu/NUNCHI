9aaf47244db4928a418a3ba766f7f6debf2d4fa0
JO HYODONG <160843817+hyodongg@users.noreply.github.com>
2026-04-06 18:39:55 +0900

    [Feat] 메뉴 DTO

--- src/main/java/dgu/capstone/nunchi/domain/menu/dto/response/MenuResponse.java
+++ src/main/java/dgu/capstone/nunchi/domain/menu/dto/response/MenuResponse.java
@@ -1,4 +1,16 @@
 package dgu.capstone.nunchi.domain.menu.dto.response;
 
-public record MenuResponse() {
+import dgu.capstone.nunchi.domain.menu.entity.Menu;
+
+public record MenuResponse(Long menuId, String name, Integer price, Boolean isSoldOut, String imageUrl) {
+
+    public static MenuResponse from(Menu menu) {
+        return new MenuResponse(
+                menu.getMenuId(),
+                menu.getName(),
+                menu.getPrice(),
+                menu.getIsSoldOut(),
+                menu.getImageUrl()
+        );
+    }
 }
