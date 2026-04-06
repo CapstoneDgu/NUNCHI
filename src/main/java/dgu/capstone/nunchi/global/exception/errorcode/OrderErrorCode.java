d0574464ca889d3a9bf00041983f7b58df6f0fc1
JO HYODONG <160843817+hyodongg@users.noreply.github.com>
2026-04-06 18:51:00 +0900

    [Feat] Order 에러 처리

---
+++ src/main/java/dgu/capstone/nunchi/global/exception/errorcode/OrderErrorCode.java
@@ -0,0 +1,38 @@
+package dgu.capstone.nunchi.global.exception.errorcode;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum OrderErrorCode implements ErrorCode {
+
+    /**
+     * 400 BAD_REQUEST
+     */
+    ORDER_ALREADY_CONFIRMED(HttpStatus.BAD_REQUEST, 400, "이미 확정된 주문입니다."),
+
+    /**
+     * 404 NOT_FOUND
+     */
+    NOT_FOUND_ORDER(HttpStatus.NOT_FOUND, 404, "존재하지 않는 주문입니다."),
+    NOT_FOUND_ORDER_ITEM(HttpStatus.NOT_FOUND, 404, "존재하지 않는 주문 항목입니다.");
+
+    private final HttpStatus status;
+    private final int code;
+    private final String msg;
+
+    OrderErrorCode(HttpStatus status, int code, String msg) {
+        this.status = status;
+        this.code = code;
+        this.msg = msg;
+    }
+
+    @Override
+    public HttpStatus getStatus() { return status; }
+
+    @Override
+    public int getCode() { return code; }
+
+    @Override
+    public String getMsg() { return msg; }
+}
