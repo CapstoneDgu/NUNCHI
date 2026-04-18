ba2a78bb7196325f9d3e2f08b24672e62c1c6aba
JO HYODONG <160843817+hyodongg@users.noreply.github.com>
2026-04-18 21:33:07 +0900

    [Feat] Repository Layer

--- src/main/java/dgu/capstone/nunchi/domain/session/repository/AiToolCallLogRepository.java
+++ src/main/java/dgu/capstone/nunchi/domain/session/repository/AiToolCallLogRepository.java
@@ -3,5 +3,9 @@ package dgu.capstone.nunchi.domain.session.repository;
 import dgu.capstone.nunchi.domain.session.entity.AiToolCallLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AiToolCallLogRepository extends JpaRepository<AiToolCallLog, Long> {
+
+    List<AiToolCallLog> findAllBySessionSessionIdOrderByCreatedAtAsc(Long sessionId);
 }
