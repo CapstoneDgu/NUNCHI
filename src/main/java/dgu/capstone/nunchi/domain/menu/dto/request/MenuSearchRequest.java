812ecb3962ceeb7f33041303908bffeb911161da
JO HYODONG <160843817+hyodongg@users.noreply.github.com>
2026-05-06 00:54:52 +0900

    [Feat] 메뉴 이름 검색 Request DTO

--- src/main/java/dgu/capstone/nunchi/domain/menu/dto/request/MenuSearchRequest.java
+++ src/main/java/dgu/capstone/nunchi/domain/menu/dto/request/MenuSearchRequest.java
@@ -1,4 +1,5 @@
 package dgu.capstone.nunchi.domain.menu.dto.request;
 
-public record MenuSearchRequest() {
-}
+public record MenuSearchRequest(
+        String name   // 메뉴 이름 부분 검색 (LIKE %name%)
+) {}
